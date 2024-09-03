c9aae21852737b710e32a47d8ff48612561eb904
Sachin Yadav <sny2771999@gmail.com>
2024-09-04 00:48:54 +0530

    have done basic setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const { serverConfig } = require('./config/index');
+
+const app = express();
+
+(async function setupAndRunServer() {
+    app.listen(function(err) {
+        if(!err){
+            console.log(`sevrer is running fine on port:  ${serverConfig.PORT}`)
+        }
+    })
+})()
